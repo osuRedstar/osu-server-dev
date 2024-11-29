d9ed68a16dfe796e76d3dcf12be046309e549b7d
전강현 <aniren@debian.moe>
2024-11-30 02:31:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,29 @@ from common.db import dbConnector
 from multiprocessing.pool import ThreadPool
 from objects import glob
 import redis
+from helpers.functions import IDM
 
 from handlers import MainHandler
+from handlers import faviconHandler
 #from helpers import getmmdb
-
-glob.pool = ThreadPool(4)
 
 def make_app():
     return tornado.web.Application([
+        #(r"/favicon.ico", tornado.web.StaticFileHandler, {"path": "/"}),
+        (r"/favicon.ico", faviconHandler.handler),
         (r"/", MainHandler.handler),
     ])
 
 if __name__ == "__main__":
     #getmmdb.dl() #config 설정완료시 활성화 하기
 
+    glob.pool = ThreadPool(4) #config.ini에 해당 값 있음
+
     #glob.db = dbConnector.db(glob.conf.config["db"]["host"], glob.conf.config["db"]["username"], glob.conf.config["db"]["password"], glob.conf.config["db"]["database"], int(glob.conf.config["db"]["workers"]))
-    glob.db = dbConnector.db("127.0.0.1", "osu", "821059401241", "redstar", 4)
+    glob.db = dbConnector.db("127.0.0.1", "osu", "pw", "redstar", 4)
 
     #glob.redis = redis.Redis(glob.conf.config["redis"]["host"], glob.conf.config["redis"]["port"], glob.conf.config["redis"]["database"], glob.conf.config["redis"]["password"])
-    glob.redis = redis.Redis("127.0.0.1", 6379, 0, 821059401241)
+    glob.redis = redis.Redis("127.0.0.1", 6379, 0, "pw")
     glob.redis.ping()
 
 
